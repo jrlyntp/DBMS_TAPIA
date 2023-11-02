21a1038a7da47a6d58ab47c560806826c6670ea3
jrlyntp <tapiajeralyn2@gmail.com>
2023-11-02 18:53:59 +0800

    j

--- demo-app/routes/web.php
+++ demo-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    
 });
 Route::prefix('/products')->group(function(){
     Route::get('/', [ProductController::class, 'index'])->name('products.index');
